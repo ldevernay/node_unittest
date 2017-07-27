f40913d693c9da7bad333532e6d66f94829f2941
ldevernay <boblehamster@yahoo.fr>
2017-07-27 16:20:38 +0200

    passing tests

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -12,3 +12,22 @@ it('should square a number', () => {
   let result = utils.square(11);
   expect(result).toBeA('number').toEqual(121);
 });
+
+it('should expect some values', () => {
+  expect(12).toNotBe(11);
+});
+
+it('should be the same array', () => {
+  // All these tests pass :
+  // expect({name:'Bob'}).toNotBe({name:'Bob'});
+  // expect({name:'Bob'}).toEqual({name:'Bob'});
+  // expect([2,3,4]).toInclude(4);
+  // expect([2,3,4]).toExclude(1);
+  expect({
+    name: 'Bob',
+    age: 34,
+    location: 'StGo'
+  }).toInclude({
+    age: 34
+  });
+});
