4401d902bfe8ef230454bcd7a61d68837717527a
ldevernay <boblehamster@yahoo.fr>
2017-07-27 16:13:01 +0200

    isNumber

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -6,12 +6,9 @@ const utils = require('./utils');
 it('should add two numbers', () => {
   let result = utils.add(33, 11);
   expect(result).toBeA('number').toEqual(44);
-  // if(result !== 44){
-  //   throw new Error(`Expected 44, got ${result}`);
-  // }
 });
 
 it('should square a number', () => {
   let result = utils.square(11);
-  expect(result).toEqual(121);
+  expect(result).toBeA('number').toEqual(121);
 });
